7f6511d0abe7e76a4dc954a539248c92470ce0c5
DuanVH <vuduanuet@gmail.com>
2018-06-28 18:01:54 +0700

    June 28, DuanVH

--- app/src/main/java/com/example/gem/tv/VideoDetailsFragment.java
+++ app/src/main/java/com/example/gem/tv/VideoDetailsFragment.java
@@ -1,10 +1,21 @@
 package com.example.gem.tv;
 
 import android.app.Activity;
+import android.support.v17.leanback.app.DetailsFragment;
 
 /**
  * Created by gem on 6/28/18.
  */
 
-public class VideoDetailsFragment extends Activity {
+public class VideoDetailsFragment extends DetailsFragment {
+
+  private static final String TAG = VideoDetailsFragment.class.getSimpleName();
+
+  private static final int DETAIL_THUMB_WIDTH = 274;
+  private static final int DETAIL_THUMB_HEIGHT = 274;
+
+  private static final String MOVIE = "movie";
+
+
+
 }
